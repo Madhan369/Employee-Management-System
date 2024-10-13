9d85fa18cfbdd41eaa7fc9ed3981f4796a2491ae
Madhan369 <145043781+Madhan369@users.noreply.github.com>
2024-10-13 23:17:40 +0530

    First Commit

---
+++ ems/Employee1.java
@@ -0,0 +1,42 @@
+package ems;
+
+public class Employee1 {
+	int id;
+	String name;
+	double salary;
+	String designation;
+
+	public Employee1(int id, String name, double salary, String designation) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.designation = designation;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", designation=" + designation + "]";
+	}
+
+	public void setName(String name) {
+		this.name = name;
+
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+}
